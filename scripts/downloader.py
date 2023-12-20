8ce70cb241ee188c22de6b8645862f98fa6c8258
miyumiyuv <32394524+miyumiyuv@users.noreply.github.com>
2023-12-20 22:07:25 +0300

    Update downloader.py

--- scripts/downloader.py
+++ scripts/downloader.py
@@ -11,7 +11,24 @@ import re
 import tqdm
 from PIL import Image
 
-# Klasör içeriğini listeleme
+def format_file_size(size):
+    for unit in ['B', 'KB', 'MB', 'GB', 'TB']:
+        if size < 1024:
+            return f"{size:.2f}{unit}"
+        size /= 1024
+    return f"{size:.2f}TB"
+
+def get_file_details(path):
+    try:
+        stat = os.stat(path)
+        return {
+            "Boyut": format_file_size(stat.st_size),
+            "Son Değiştirilme": time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(stat.st_mtime))
+        }
+    except Exception as e:
+        return {"Hata": str(e)}
+
+# Dosya işlemleri
 def list_files(directory):
     try:
         files = os.listdir(directory)
@@ -19,7 +36,6 @@ def list_files(directory):
     except Exception as e:
         return str(e)
 
-# Dosya kopyalama
 def copy_file(source, destination):
     try:
         shutil.copy(source, destination)
@@ -27,7 +43,6 @@ def copy_file(source, destination):
     except Exception as e:
         return str(e)
 
-# Dosya silme
 def delete_file(file_path):
     try:
         os.remove(file_path)
@@ -35,7 +50,6 @@ def delete_file(file_path):
     except Exception as e:
         return str(e)
 
-# Dosya taşıma
 def move_file(source, destination):
     try:
         shutil.move(source, destination)
@@ -44,37 +58,49 @@ def move_file(source, destination):
         return str(e)
 
 def on_ui_tabs():
-    with gr.Blocks(analytics_enabled=False) as downloader:
+    with gr.Blocks(analytics_enabled=False) as filemanager:
         create_parent_dirs = gr.Checkbox(True, label="Create parent model directories if they do not exist (this may result in unintended consequences if you mistype something!)", interactive=True)
-        with gr.Tab("File Manager"):
-            with gr.Column():
-                with gr.Row():
-                    list_button = gr.Button("List Files")
-                    directory_input = gr.Textbox(label="Directory", value=".")
-                    list_output = gr.Textbox(label="Files", multiline=True, interactive=False)
-                    list_button.click(list_files, inputs=[directory_input], outputs=[list_output])
+            with gr.Tab("File Manager"):
+        with gr.Column():
+            # Dosya Listeleme
+            with gr.Row():
+                list_button = gr.Button("List Files")
+                directory_input = gr.Textbox(label="Directory", value=".")
+                list_output = gr.Textbox(label="Files", multiline=True, interactive=False)
+                list_button.click(list_files, inputs=[directory_input], outputs=[list_output])
 
-                with gr.Row():
-                    copy_source = gr.Textbox(label="Copy - Source File")
-                    copy_destination = gr.Textbox(label="Copy - Destination File")
-                    copy_button = gr.Button("Copy")
-                    copy_output = gr.Textbox(label="Result", interactive=False)
-                    copy_button.click(copy_file, inputs=[copy_source, copy_destination], outputs=[copy_output])
+            # Dosya Kopyalama
+            with gr.Row():
+                copy_source = gr.Textbox(label="Copy - Source File")
+                copy_destination = gr.Textbox(label="Copy - Destination File")
+                copy_button = gr.Button("Copy")
+                copy_output = gr.Textbox(label="Result", interactive=False)
+                copy_button.click(copy_file, inputs=[copy_source, copy_destination], outputs=[copy_output])
 
-                with gr.Row():
-                    delete_file_input = gr.Textbox(label="Delete - File Path")
-                    delete_button = gr.Button("Delete")
-                    delete_output = gr.Textbox(label="Result", interactive=False)
-                    delete_button.click(delete_file, inputs=[delete_file_input], outputs=[delete_output])
+            # Dosya Silme
+            with gr.Row():
+                delete_file_input = gr.Textbox(label="Delete - File Path")
+                delete_button = gr.Button("Delete")
+                delete_output = gr.Textbox(label="Result", interactive=False)
+                delete_button.click(delete_file, inputs=[delete_file_input], outputs=[delete_output])
 
-                with gr.Row():
-                    move_source = gr.Textbox(label="Move - Source File")
-                    move_destination = gr.Textbox(label="Move - Destination File")
-                    move_button = gr.Button("Move")
-                    move_output = gr.Textbox(label="Result", interactive=False)
-                    move_button.click(move_file, inputs=[move_source, move_destination], outputs=[move_output])
+            # Dosya Taşıma
+            with gr.Row():
+                move_source = gr.Textbox(label="Move - Source File")
+                move_destination = gr.Textbox(label="Move - Destination File")
+                move_button = gr.Button("Move")
+                move_output = gr.Textbox(label="Result", interactive=False)
+                move_button.click(move_file, inputs=[move_source, move_destination], outputs=[move_output])
 
-    return (downloader, "Downloader", "downloader"),
+            # Dosya Detayları
+            with gr.Row():
+                file_path_input = gr.Textbox(label="Dosya/Klasör Yolu")
+                file_details_button = gr.Button("Dosya Detayları")
+                file_details_output = gr.Textbox(label="Detaylar", multiline=True, interactive=False)
+                file_details_button.click(lambda path: "\n".join([f"{k}: {v}" for k, v in get_file_details(path).items()]), inputs=[file_path_input], outputs=[file_details_output])
+
+
+    return (filemanager, "File Manager", "filemanager"),
 
 
 script_callbacks.on_ui_tabs(on_ui_tabs)
